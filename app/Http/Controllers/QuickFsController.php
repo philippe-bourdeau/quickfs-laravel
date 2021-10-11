e642b0f5eaef3b4ec83d9ded1e03450a13d535f0
stainless <bourdeau.phil@gmail.com>
2021-10-11 15:03:57 -0400

    add currency to data

--- app/Http/Controllers/QuickFsController.php
+++ app/Http/Controllers/QuickFsController.php
@@ -43,6 +43,7 @@ class QuickFsController extends Controller
             'data' => [
                 'market_cap' => sprintf('QFS(%s,mkt_cap)', $ticker),
                 'price' => sprintf('QFS(%s,price)', $ticker),
+                'currency' => sprintf('QFS(%s,currency)', $ticker),
                 'period_end_date' => sprintf('QFS(%s,period_end_date,FY-9:FY)', $ticker),
                 'revenue' => sprintf('QFS(%s,revenue,FY-9:FY)', $ticker),
                 'net_income' => sprintf('QFS(%s,net_income,FY-9:FY)', $ticker),
