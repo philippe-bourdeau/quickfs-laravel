654231d5a8538fc7c38bad3a9ac2356a66f67011
stainless <bourdeau.phil@gmail.com>
2022-09-06 00:24:19 -0400

    improve cors configuration

--- config/cors.php
+++ config/cors.php
@@ -15,11 +15,11 @@ return [
     |
     */
 
-    'paths' => ['api/*', 'sanctum/csrf-cookie'],
+    'paths' => ['*', 'sanctum/csrf-cookie'],
 
     'allowed_methods' => ['*'],
 
-    'allowed_origins' => ['*'],
+    'allowed_origins' => ['*.stripe.com'],
 
     'allowed_origins_patterns' => [],
 
